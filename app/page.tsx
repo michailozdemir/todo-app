ef10fdc7964c506a5a2ef132b4232ce9454fafad
michail <michail.ozdemir@gmail.com>
2025-02-12 15:40:18 +0100

    Update Tailwind - done

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <section className="py-16 w-full max-w-7xl sm:py-32 2xl:py-64 text-center bg-hero-bg dark:bg-hero-bg-dark bg-no-repeat bg-600 bg-bottom sm:bg-center sm:bg-1100 2xl:bg-auto">
       <div className="px-4">
-        <h1 className="text-4xl font-medium text-transparent sm:text-7xl bg-clip-text bg-gradient-to-r from-foreground to-slate-500 dark:from-slate-200 dark:to-primary">
+        <h1 className="text-4xl font-medium text-transparent sm:text-7xl bg-clip-text bg-linear-to-r from-foreground to-slate-500 dark:from-slate-200 dark:to-primary">
           Todo.easy - your daily todos
         </h1>
         <p className="mt-4 text-xl text-zinc-500">
